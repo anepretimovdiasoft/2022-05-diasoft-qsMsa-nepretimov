2f7b47b4843e895a7390f9af33769de4ad4734ee
anepretimovdiasoft <94528503+anepretimovdiasoft@users.noreply.github.com>
2022-05-27 18:07:26 +0300

    module-6 result v1

--- src/main/java/ru/diasoft/service/PersonServiceImpl.java
+++ src/main/java/ru/diasoft/service/PersonServiceImpl.java
@@ -2,6 +2,7 @@ package ru.diasoft.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.diasoft.dao.PersonDao;
 import ru.diasoft.domain.Person;
 import ru.diasoft.exception.NotFindPersonException;
@@ -15,11 +16,13 @@ public class PersonServiceImpl implements PersonService {
     private final PersonDao personDao;
 
     @Override
+    @Transactional(readOnly = true)
     public List<Person> getAll() {
         return personDao.findAll();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Person getPersonById(long id) {
         return personDao
                 .findById(id)
@@ -27,11 +30,13 @@ public class PersonServiceImpl implements PersonService {
     }
 
     @Override
+    @Transactional
     public Person addPerson(Person person) {
         return personDao.save(person);
     }
 
     @Override
+    @Transactional
     public Person updatePerson(long id, String name) {
 
         return personDao.save(
@@ -43,6 +48,7 @@ public class PersonServiceImpl implements PersonService {
     }
 
     @Override
+    @Transactional
     public void deletePersonById(long id) {
 
         personDao.deleteById(id);
